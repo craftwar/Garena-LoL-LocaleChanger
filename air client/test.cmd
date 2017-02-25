0f4bbb094428fb0c0def49b34e3f74c8a21575a8
craftwar <coo.ky@hotmail.com>
2017-02-25 20:41:52 +0800

    fix batch script

--- air client/test.cmd
+++ air client/test.cmd
@@ -1,11 +1,9 @@
-rem echo off
-Setlocal EnableDelayedExpansion
+echo off
 IF [%1] NEQ [] (
-	set base = %~1
+	set base=%~1
 ) ELSE (
-	set base = %cd%
+	set base=%cd%
 )
-
-rem dir "!base!\GameData\Apps\LoLTW\Air\css\fonts_en_US.swf"
-mklink /H "%1\GameData\Apps\LoLTW\Air\css\fonts_en_US.swf" "%1\GameData\Apps\LoLTW\Air\css\fonts_zh_TW.swf"
-copy /Y locale.properties "%1\GameData\Apps\LoLTW\Air\locale.properties"
+set base
+mklink /H "%base%\GameData\Apps\LoLTW\Air\css\fonts_en_US.swf" "%base%\GameData\Apps\LoLTW\Air\css\fonts_zh_TW.swf"
+copy /Y locale.properties "%base%\GameData\Apps\LoLTW\Air\locale.properties"

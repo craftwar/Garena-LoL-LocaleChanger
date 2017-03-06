0f44f357831e3fcbb76510bb6010a1477031b89f
craftwar <coo.ky@hotmail.com>
2017-03-06 15:12:48 +0800

    fix locale.properties read-only attribute
    use xcopy to keep read-only

--- air client/Lobby_eng.cmd
+++ air client/Lobby_eng.cmd
@@ -5,4 +5,4 @@ IF [%1] NEQ [] (
 	set base=%cd%
 )
 mklink /H "%base%\GameData\Apps\LoLTW\Air\css\fonts_en_US.swf" "%base%\GameData\Apps\LoLTW\Air\css\fonts_zh_TW.swf"
-copy /Y /A "%~dp0locale.properties" "%base%\GameData\Apps\LoLTW\Air\locale.properties"
+xcopy /K /Y "%~dp0locale.properties" "%base%\GameData\Apps\LoLTW\Air\locale.properties"*

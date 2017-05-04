af67f29c5dd8adb7a414ca68a6e34c087086bad7
craftwar <coo.ky@hotmail.com>
2017-05-04 10:22:07 +0800

    fix

--- cef client/Lobby_eng_restore.cmd
+++ cef client/Lobby_eng_restore.cmd
@@ -4,4 +4,4 @@ IF [%1] NEQ [] (
 ) ELSE (
 	set base=%cd%
 )
-move "%base%\backup_zh_TW" "%base%\GameData\Apps\LoLTW\LeagueClient\Plugins\rcp-be-lol-game-data\zh_TW-assets.wad"
+move "%base%\backup_zh_TW\zh_TW-assets.wad" "%base%\GameData\Apps\LoLTW\LeagueClient\Plugins\rcp-be-lol-game-data\zh_TW-assets.wad"

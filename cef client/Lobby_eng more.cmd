e509dfe4a219df077dea7a5719f045516c82a093
craftwar <coo.ky@hotmail.com>
2017-05-17 18:29:44 +0800

    add : remove logitech related dlls script

---
+++ cef client/Lobby_eng more.cmd
@@ -0,0 +1,10 @@
+echo off
+IF [%1] NEQ [] (
+	set base=%~1
+) ELSE (
+	set base=%cd%
+)
+md "%base%\backup_zh_TW"
+move "%base%\GameData\Apps\LoLTW\LeagueClient\Plugins\rcp-be-lol-game-data\zh_TW-assets.wad" "%base%\backup_zh_TW"
+move "%base%\GameData\Apps\LoLTW\Game\LogitechGkey.dll" "%base%\backup_zh_TW"
+move "%base%\GameData\Apps\LoLTW\Game\LogitechLed.dll" "%base%\backup_zh_TW"
--- cef client/Lobby_eng_restore.cmd
+++ cef client/Lobby_eng_restore.cmd
@@ -5,3 +5,5 @@ IF [%1] NEQ [] (
 	set base=%cd%
 )
 move "%base%\backup_zh_TW\zh_TW-assets.wad" "%base%\GameData\Apps\LoLTW\LeagueClient\Plugins\rcp-be-lol-game-data\zh_TW-assets.wad"
+move "%base%\backup_zh_TW\LogitechGkey.dll" "%base%\GameData\Apps\LoLTW\Game\LogitechGkey.dll" 
+move "%base%\backup_zh_TW\LogitechLed.dll" "%base%\GameData\Apps\LoLTW\Game\LogitechLed.dll"
